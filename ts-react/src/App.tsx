dce3cfc5c909d78e755d7d38033bb300f968d79d
wflixu <wflixu@yeah.net>
2019-05-04 17:03:59 +0800

    tic

--- ts-react/src/App.tsx
+++ ts-react/src/App.tsx
@@ -1,26 +1,45 @@
 import * as React from 'react';
+
+import { createStore } from 'redux';
+import counter from './reducers';
+
+const store = createStore(counter);
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch({ type: 'INCREMENT' })
+// 1
+store.dispatch({ type: 'INCREMENT' })
+// 2
+store.dispatch({ type: 'DECREMENT' })
+
+// import { Board } from "./components/Board";
+// import { RestartBtn } from "./components/RestartBtn";
+// import { GameStateBar } from "./components/GameStateBar";
+
+import 'bootstrap/dist/css/bootstrap.css'
+import Counter from './components/count';
 import './App.css';
 
 
-interface IProps {
-  color: string,
-  size?: string,
-}
-interface IState {
-  count: number,
-}
 
-class App extends React.Component<IProps, IState> {
-  public readonly state: Readonly<IState> = {
-    count: 1,
-  }
-  public componentDidMount () {
-    this.setState({count:2})
-  }
+class App extends React.Component<{}, {}> {
+         
 
   public render () {
     return (
-      <div>Hello world</div>
+      <div className="app"> 
+              <Counter  value={store.getState()}
+                 onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+                onDecrement={() => store.dispatch({ type: 'DECREMENT' })}/>
+                {/* <Board /> 
+                <div >
+                    <span className="description t1"> Player(X) </span>                
+                    <span className="description t2"> Computer(O) </span>
+                    <RestartBtn />     
+                   <GameStateBar />     
+                </div>
+                     */}
+            </div> 
     )
   }
 }
