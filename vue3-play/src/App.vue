e5e1784ed978b7005e4494536d6f3d3a279565f9
wflixu <wflixu@yeah.net>
2020-10-25 17:41:54 +0800

    vue3 play

---
+++ vue3-play/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <Life/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Life from './components/life.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Life
+  }
+}
+</script>
---
+++ vue3-play/src/components/life.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>生命周期</h2>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+export default {
+  setup() {
+    onMounted(() => {
+      console.log('component is mounted!');
+    });
+  },
+};
+</script>
